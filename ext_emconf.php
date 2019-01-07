514595ae526d178499f6225c07e37bd92081a03f
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2019-01-07 23:19:22 +0100

    [TASK] Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Tactician command bus wrapper',
+    'description' => 'Tactician command bus wrapper',
+    'category' => 'misc',
+    'author' => 'Sebastian Schreiber',
+    'author_email' => 'breakpoint@schreibersebastian.de',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'Ssch\\T3Tactician\\' => 'Classes',
+        ],
+    ],
+    'autoload-dev' => [
+        'psr-4' => [
+            'Ssch\\T3Tactician\\Tests\\' => 'Tests',
+        ],
+    ],
+];
---
+++ ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+$boot = function () {
+
+};
+
+$boot();
+unset($boot);
