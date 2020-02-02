11f7d191853465e01c88967991788354537b1cb8
Sebastian Schreiber <breakpoint@schreibersebastian.de>
2020-02-02 21:24:52 +0100

    [TASK] Activate functional tests
    - In order to test the whole configuration setup, we activate functional tests again

---
+++ Tests/Functional/Fixtures/Extensions/t3_tactician_test/Configuration/CommandBus.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'testing' => [
+        'commandHandler' => [
+            \Ssch\T3Tactician\Tests\Unit\Fixtures\Command\AddTaskCommand::class => \Ssch\T3Tactician\Tests\Unit\Fixtures\Handler\AddTaskHandler::class,
+        ],
+        'middleware' => [
+            \Ssch\T3Tactician\Middleware\LoggingMiddleware::class,
+            \Ssch\T3Tactician\Middleware\ValidatorMiddleware::class,
+        ],
+    ],
+    'testingMethodNameInflector' => [
+        'commandHandler' => [
+            \Ssch\T3Tactician\Tests\Unit\Fixtures\Command\AnotherTaskCommand::class => \Ssch\T3Tactician\Tests\Unit\Fixtures\Handler\AnotherTaskHandler::class,
+        ],
+        'inflector' => [
+            \League\Tactician\Handler\MethodNameInflector\InvokeInflector::class
+        ]
+    ],
+    'testingScheduler' => [
+        'commandHandler' => [
+            \Ssch\T3Tactician\Tests\Unit\Fixtures\Command\DummyScheduledCommand::class => \Ssch\T3Tactician\Tests\Unit\Fixtures\Handler\DummyScheduledHandler::class
+        ],
+        'middleware' => [
+            \Ssch\T3Tactician\Middleware\SchedulerMiddleware::class
+        ],
+    ]
+];
