a5a62105206eda7a7d96cda8fc7229ae09032e9e
Sebastian Schreiber <me@schreibersebastian.de>
2022-11-03 11:02:51 +0100

    TASK: Fix wrong DI configuration

--- Classes/HandlerLocator/HandlerLocator.php
+++ Classes/HandlerLocator/HandlerLocator.php
@@ -29,7 +29,7 @@ final class HandlerLocator implements HandlerLocatorInterface
     private $objectManager;
 
     /**
-     * @var CommandBusConfiguration
+     * @var CommandBusConfigurationInterface
      */
     private $commandBusConfiguration;
 
--- Configuration/Services.php
+++ Configuration/Services.php
@@ -1,18 +1,21 @@
 <?php
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use League\Tactician\CommandBus;
+use League\Tactician\Handler\CommandNameExtractor\ClassNameExtractor;
+use Ssch\T3Tactician\Factory\CommandBusFactory;
+use Ssch\T3Tactician\Factory\CommandBusFactoryInterface;
+use Ssch\T3Tactician\Middleware\MiddlewareHandlerResolver;
+use Ssch\T3Tactician\Middleware\MiddlewareHandlerResolverInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-return static function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder): void {
+return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-    $services->defaults()
-        ->public()
-        ->autowire()
-        ->autoconfigure();
 
-    $services->load('Ssch\\T3Tactician\\', __DIR__ . '/../Classes/')->exclude([
-        __DIR__ . '/../Classes/Middleware/InvalidCommandException.php',
-        __DIR__ . '/../Classes/Validator/NoValidatorFoundException.php',
-        __DIR__ . '/../Classes/CommandAlreadyAssignedToHandlerException.php',
-    ]);
+    $services->set(CommandBusFactory::class)->autowire();
+    $services->set(MiddlewareHandlerResolver::class)->autowire();
+    $services->set(CommandBus::class);
+    $services->set(ClassNameExtractor::class);
+
+    $services->alias(MiddlewareHandlerResolverInterface::class, MiddlewareHandlerResolver::class);
+    $services->alias(CommandBusFactoryInterface::class, CommandBusFactory::class);
 };
